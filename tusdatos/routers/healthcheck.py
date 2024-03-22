0bd9f7fb9e705558480e2b69112efd829dd6d60f
Gabriel Morales <dev@darklock.maskmy.id>
2024-03-21 22:55:21 -0500

    Linter and formater.

--- tusdatos/routers/healthcheck.py
+++ tusdatos/routers/healthcheck.py
@@ -1,15 +1,17 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy import text
 from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import Session
 
-from tusdatos.core.database import client as mongo_client
-from tusdatos.core.database import get_db
+from tusdatos.core.database import client as mongo_client, get_db
 
 router = APIRouter(tags=["Health Checks"])
 
 
-@router.get("/healthcheck")
+@router.get(
+    "/healthcheck",
+    status_code=status.HTTP_200_OK,
+)
 async def healthcheck(postgres_db: Session = Depends(get_db)) -> dict:
     try:
         await postgres_db.execute(text("SELECT 1"))
